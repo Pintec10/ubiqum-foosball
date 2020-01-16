b13b6f33f9f5c1086d7bc1924a4b8e3973e2a3e3
ROBERTO MILANI <robimilani@gmail.com>
2020-01-16 18:24:45 +0100

    200116 first commit

---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app-bar app color="grey darken-1" dark class="test">
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-img alt="Ubiqum foosball logo" src="@/assets/futbolin_logo_long.jpg" />
+    <v-btn icon>
+      <v-icon>mdi-account</v-icon>
+      <!-- replace icon with profile picture -->
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  //
+};
+</script>
+
+<style>
+.test {
+  border: 1px solid red !important;
+}
+</style>
