13b4fdfc267625f11e3765165db5af46eb0292c6
ROBERTO MILANI <robimilani@gmail.com>
2020-01-20 19:15:09 +0100

    200120 sprint 1 complete. Add: home, login, createAccount

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,22 @@
 <template>
   <v-container fluid class="fill-height px-auto">
     <v-row class="d-flex align-center justify-center">
-      <v-col class="col-12 col-sm-6 col-md-12 pa-6 d-flex align-center justify-center">
+      <v-col class="col-12 pa-6 d-flex align-center justify-center">
         <v-img src="@/assets/futbolin_logo_square.svg" max-width="30vh"></v-img>
       </v-col>
 
-      <v-col class="col-12 col-sm-6 col-md-12 pa-6 text-center">
-        <v-row>
-          <v-col class="col-1 col-md-4"></v-col>
-          <v-col class="col-10 col-md-4">
-            <v-form>
-              <v-text-field v-model="username" label="Username" required></v-text-field>
-              <v-text-field v-model="password" label="Password" required></v-text-field>
-              <v-btn color="grey darken-4 ma-3">Login</v-btn>
-              <v-btn color="grey darken-4 ma-3">Create account</v-btn>
-            </v-form>
-          </v-col>
-          <v-col class="col-1 col-md-4"></v-col>
-        </v-row>
+      <v-col class="col-12 pa-6 d-flex align-center justify-center">
+        <v-btn to="/login" outlined class="ma-3">Login</v-btn>
+      </v-col>
+
+      <v-col class="col-12 pa-6 d-flex align-center justify-center">
+        <v-btn to="/create-account" outlined class="ma-3">Create Account</v-btn>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-//import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: "home",
   components: {
